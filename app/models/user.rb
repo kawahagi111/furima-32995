b01c5f305f5e490a65693569bf113181fc21911d
kawahagi111 <0tg0cw0b78v3x8t@au.com>
2021-01-04 14:10:15 +0900

    ユーザー管理機能の実装3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,16 @@ class User < ApplicationRecord
        has_many :buys
        
        validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-      with_options presence: true do
-       validates :nickname         
-       validates :last_name, format: {with: /\A[ぁ-ん一-龥]+\z/} 
-       validates :first_name,        format: {with: /\A[ぁ-ん一-龥]+\z/}
-       validates :last_name_kana,    format: {with: /\p{katakana}/}
-       validates :first_name_kana,   format: {with: /\p{katakana}/}
+    with_options presence: true do
+       validates :nickname
        validates :birthday
-      end
+       with_options format: {with: /\A[ぁ-ん一-龥]+\z/} do
+       validates :last_name
+       validates :first_name
+       end
+       with_options format: {with: /\p{katakana}/} do
+       validates :last_name_kana
+       validates :first_name_kana
+       end
+    end
 end
