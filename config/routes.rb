853d857178128859357134f9acc3c9119204ce80
kawahagi111 <0tg0cw0b78v3x8t@au.com>
2021-01-07 16:35:37 +0900

    商品情報編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "items#index"
 
 
-  resources :items, only: [:update, :create, :index, :new, :show]
+  resources :items, only: [:update, :create, :index, :new, :show, :edit]
 end
